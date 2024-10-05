00bc1855d77796cad92940c428e0d016e009ba45
Riceball LEE <snowyu.lee@gmail.com>
2024-10-05 16:16:02 +0800

    fix: regex test can not work if output is regex directly

--- src/lib/to-match-object.ts
+++ src/lib/to-match-object.ts
@@ -5,7 +5,7 @@
 //   JestExtend,
 // } from '@vitest/expect'
 import { getKeysPath, isRegExp, toRegExp } from '@isdk/ai-tool'
-import { cloneDeep, get as getByPath } from 'lodash-es'
+import { get as getByPath } from 'lodash-es'
 import colors from 'ansicolor'
 import { diffChars } from 'diff'
 
@@ -18,35 +18,53 @@ import { diffChars } from 'diff'
 
 // const expect = chai.expect as any
 
-export function toMatchObject(actual: any, expected: any, failedKeys: string[] = []) {
-  expected = cloneDeep(expected)
-  const keys = getKeysPath(expected)
-  for (const k of keys) {
-    const v = getByPath(expected, k)
-    const actualValue = getByPath(actual, k)
-    if (isRegExp(v)) {
-      const regEx = toRegExp(v)
-      if (!regEx.test(actualValue)) {failedKeys.push(k + ': /' + regEx.source + '/' + regEx.flags + `.test(${JSON.stringify(actualValue)}) failed`)}
-    } else if (typeof v === 'string') {
-      if (typeof actualValue !== 'string' || !actualValue.includes(v.trim())) {
-        if (typeof actualValue === 'string') {
-          const diff = diffChars(v, actualValue)
-          // const diffStr = diff.map(d => d.added ? `+${d.value}` : d.removed ? `-${d.value}` : d.value).join('')
-          const diffStr = diff.map(d =>
-            d.added ? colors.green('+'+d.value) :
-            d.removed ? colors.red('-'+d.value) : colors.darkGray(d.value)).join('')
-          failedKeys.push(k+ ':' + diffStr)
-        } else {
-          failedKeys.push(k + ': ' + JSON.stringify(actualValue) + ' != ' + JSON.stringify(v))
-        }
+function matchValue(actual: any, expected: any, failedKeys: string[] = [], key: string = '') {
+  const vType = typeof expected
+  const kStr = key ? key + ': ' : ''
+  if (typeof actual === 'string') {actual = actual.trim()}
+  if (isRegExp(expected)) {
+    const regEx = toRegExp(expected)
+    console.log('🚀 ~ matchValue ~ regEx:', regEx, actual)
+    if (!regEx.test(actual)) {
+      failedKeys.push(kStr + '/' + regEx.source + '/' + regEx.flags + `.test(${JSON.stringify(actual)}) failed`)
+    }
+  } else if (vType === 'string') {
+    if (typeof actual !== 'string' || !actual.includes(expected.trim())) {
+      if (typeof actual === 'string') {
+        const diff = diffChars(expected, actual)
+        // const diffStr = diff.map(d => d.added ? `+${d.value}` : d.removed ? `-${d.value}` : d.value).join('')
+        const diffStr = diff.map(d =>
+          d.added ? colors.green('+'+d.value) :
+          d.removed ? colors.red('-'+d.value) : colors.darkGray(d.value)).join('')
+        failedKeys.push(kStr + diffStr)
+      } else {
+        failedKeys.push(kStr + JSON.stringify(actual) + ' != ' + JSON.stringify(expected))
       }
-    } else {
-      if (actualValue !== v) {failedKeys.push(k + ': ' + JSON.stringify(actualValue) + ' != ' + JSON.stringify(v))}
+    }
+  } else if (Array.isArray(expected)) {
+    if (!Array.isArray(actual)) {
+      failedKeys.push(kStr + 'the actual value is not an array: ' + JSON.stringify(actual))
+    }
+    for (let i = 0; i < expected.length; i++) {
+      const vItem = expected[i]
+      const actualItem = actual[i]
+      matchValue(actualItem, vItem, failedKeys, key + '[' + i + ']')
+    }
+  } else if (vType === 'object') {
+    const keys = getKeysPath(expected)
+    for (const k of keys) {
+      const v = getByPath(expected, k)
+      const actualValue = getByPath(actual, k)
+      matchValue(actualValue, v, failedKeys, k)
+    }
+  } else {
+    if (actual !== expected) {
+      failedKeys.push(key + ': ' + JSON.stringify(actual) + ' != ' + JSON.stringify(expected))
     }
   }
-  // if (failedKeys.length) {
-  //   throw new Error(`MisMatch: ${failedKeys.join(',')}`)
-  // }
-
   return failedKeys.length ? failedKeys : undefined
+}
+
+export function toMatchObject(actual: any, expected: any, failedKeys: string[] = [], key: string = '') {
+  return matchValue(actual, expected, failedKeys, key)
 }
