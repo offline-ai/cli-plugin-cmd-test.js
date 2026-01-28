9cf0440db3eebae40daecc623e70f4f0d077dbfb
Riceball LEE <snowyu.lee@gmail.com>
2026-01-28 20:17:07 +0800

    fix: can not replace the template var in input

--- src/lib/test-fixture-file.ts
+++ src/lib/test-fixture-file.ts
@@ -99,7 +99,7 @@ export async function* testFixtureFileInScript(fixtures: any[], {scriptFilepath,
       thisCmd.error(`fixture[${i}] missing output or outputSchema for the fixture file: ` + fixtureFilepath)
     }
     fixtureConfig = await formatObject(fixtureConfig, {data: {...input, ...fixtureConfig}, input: fixture});
-    userConfig.data = await formatObject({...input}, {data: {...getTemplateData(scriptConfig), ...input, fixtureConfig}, input: fixture});
+    userConfig.data = await formatObject({...input}, {data: {...getTemplateData(scriptConfig), ...input, ...fixtureConfig}, input: fixture});
 
     userConfig.interactive = false
     // userConfig.chatsDir = undefined
