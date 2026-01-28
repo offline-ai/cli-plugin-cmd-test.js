78bbbd9c9e110699ce7d5323ff5206da81ff5059
Riceball LEE <snowyu.lee@gmail.com>
2026-01-29 06:26:51 +0800

    refactor: use new stringifyYaml in ai-tool

--- src/lib/write-yaml-file.ts
+++ src/lib/write-yaml-file.ts
@@ -1,6 +1,5 @@
 import fs from 'fs/promises'
-// @ts-ignore
-import { stringifyYaml } from '@isdk/ai-tool-agent'
+import { stringifyYaml } from '@isdk/ai-tool'
 
 export async function writeYamlFile(filepath: string, data: any) {
   const str = stringifyYaml(data)
